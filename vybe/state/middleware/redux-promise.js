3634ea664c8f916158bbe7c7e6c9cebf98a1967f
Deus Duke <deusduke@gmail.com>
2018-01-24 20:31:54 -0800

    remove simple-promise and create own version

---
+++ vybe/state/middleware/redux-promise.js
@@ -0,0 +1,29 @@
+import { isFSA } from "flux-standard-action"
+
+function isPromise(val) {
+  return val && typeof val.then === "function"
+}
+
+export default function promiseMiddleware({ dispatch }) {
+  return next => action => {
+    if (!isFSA(action)) {
+      return isPromise(action) ? action.then(dispatch) : next(action)
+    }
+
+    if (isPromise(action.payload)) {
+      // let em know that we are loading
+      dispatch({ ...action, pending: true })
+
+      // return the promise
+      return action.payload.then(
+        result => dispatch({ ...action, payload: result }),
+        error => {
+          dispatch({ ...action, payload: error, error: true })
+          return Promise.reject(error)
+        }
+      )
+    }
+
+    return next(action)
+  }
+}
