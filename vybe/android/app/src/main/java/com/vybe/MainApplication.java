e6f1e775ae8aa27090365517e0709201b9e7d590
Deus Duke <deusduke@gmail.com>
2018-03-02 09:48:13 -0800

    remove react native fs

--- vybe/android/app/src/main/java/com/vybe/MainApplication.java
+++ vybe/android/app/src/main/java/com/vybe/MainApplication.java
@@ -3,6 +3,7 @@ package com.vybe;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.rnfs.RNFSPackage;
 import com.imagepicker.ImagePickerPackage;
 import com.RNFetchBlob.RNFetchBlobPackage;
 import com.lugg.ReactNativeConfig.ReactNativeConfigPackage;
@@ -27,6 +28,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RNFSPackage(),
             new ImagePickerPackage(),
             new RNFetchBlobPackage(),
             new ReactNativeConfigPackage(),
