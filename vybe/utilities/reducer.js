28770e9561db7be1bfd880d5da89826d94505231
Deus Duke <deusduke@gmail.com>
2018-03-01 23:19:29 -0800

    add convenience functions for reducer and test

--- vybe/utilities/reducer.js
+++ vybe/utilities/reducer.js
@@ -1,13 +1,13 @@
 import { PENDING, FULFILLED, REJECTED } from "redux-promise-middleware"
 
 export function pending(type: string) {
-  return `${type}${PENDING}`
+  return `${type}_${PENDING}`
 }
 
 export function resolve(type: string) {
-  return `${type}${FULFILLED}`
+  return `${type}_${FULFILLED}`
 }
 
 export function reject(type: string) {
-  return `${type}${REJECTED}`
+  return `${type}_${REJECTED}`
 }
