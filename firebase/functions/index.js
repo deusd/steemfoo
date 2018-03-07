ca1183e882edaff4b90dc839c53a1de737cb5059
Deus Duke <deusduke@gmail.com>
2018-03-07 00:00:23 -0800

    begin working on vybe api

--- firebase/functions/index.js
+++ firebase/functions/index.js
@@ -1,8 +1,14 @@
 const functions = require("firebase-functions")
+const steem = require("steem")
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-exports.helloWorld = functions.https.onRequest((request, response) => {
-  response.send("Hello from Firebase!")
+exports.getLatestPosts = functions.https.onRequest((request, response) => {
+  response.send("Hello from getLatestPosts!")
+})
+
+exports.getTrendingPosts = functions.https.onRequest((request, response) => {
+  response.send("Hello from getTrendingPosts!")
+})
+
+exports.getHotPosts = functions.https.onRequest((request, response) => {
+  response.send("Hello from getHotPosts!")
 })
