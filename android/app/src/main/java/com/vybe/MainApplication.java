88d64af507cd7a99351ad7c5255bc49f0bd817ee
Deus Duke <deusduke@gmail.com>
2018-03-13 22:06:09 -0700

    re-link app

--- android/app/src/main/java/com/vybe/MainApplication.java
+++ android/app/src/main/java/com/vybe/MainApplication.java
@@ -6,6 +6,9 @@ import com.facebook.react.ReactApplication;
 import com.imagepicker.ImagePickerPackage;
 import com.RNFetchBlob.RNFetchBlobPackage;
 import com.lugg.ReactNativeConfig.ReactNativeConfigPackage;
+import com.imagepicker.ImagePickerPackage;
+import com.RNFetchBlob.RNFetchBlobPackage;
+import com.lugg.ReactNativeConfig.ReactNativeConfigPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -26,6 +29,9 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new ImagePickerPackage(),
+            new RNFetchBlobPackage(),
+            new ReactNativeConfigPackage(),
             new ImagePickerPackage(),
             new RNFetchBlobPackage(),
             new ReactNativeConfigPackage()
