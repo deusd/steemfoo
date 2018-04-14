2fa7e1440ac139eba5fc761d012bef3f81dc67cb
Deus Duke <deusduke@gmail.com>
2018-04-13 19:21:00 -0700

    Add loading component spec
    [#156414843]

---
+++ components/__tests__/LoadingSpec.js
@@ -0,0 +1,21 @@
+import 'react-native'
+import { shallow } from 'enzyme'
+import React from 'react'
+import { ActivityIndicator } from 'react-native'
+import Loading from '../Loading'
+
+describe('Loading component', () => {
+  let wrapper
+
+  beforeEach(() => {
+    wrapper = shallow(<Loading />)
+  })
+
+  it('should render with no styles', () => {
+    expect(wrapper).toMatchSnapshot()
+  })
+
+  it('should have an activity indicator', () => {
+    expect(wrapper.contains(<ActivityIndicator />)).toBe(true)
+  })
+})
