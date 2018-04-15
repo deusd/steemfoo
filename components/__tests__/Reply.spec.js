9bb7fa4625d3f5c94a936bed3b622f3160c58593
Deus Duke <deusduke@gmail.com>
2018-04-15 14:18:42 -0700

    Split out replies component and test
    [#156414843]

---
+++ components/__tests__/Reply.spec.js
@@ -0,0 +1,66 @@
+import 'react-native'
+import { shallow } from 'enzyme'
+import React from 'react'
+import { Text } from 'react-native'
+import Reply from '../Reply'
+import ProfileImageThumb from '../ProfileImageThumb'
+import { PROFILE_PLACEHOLDER_IMAGE } from '../../constants'
+
+const reply = {
+  author: 'jahangirwifii',
+  authorAccount: {
+    profileImage: 'https://steemitimages.com/u/jahangirwifii/avatar',
+  },
+  body: '@tipu need again 2.5...',
+  voteCount: 0,
+}
+
+describe('Reply component', () => {
+  let wrapper
+
+  beforeEach(() => {
+    wrapper = shallow(<Reply reply={reply} />)
+  })
+
+  describe('should render with the mock reply content', () => {
+    expect(wrapper).toMatchSnapshot()
+
+    it('with the profile image', () => {
+      expect(wrapper.find('ProfileImageThumb').props().profileImage).toEqual(
+        reply.authorAccount.profileImage
+      )
+    })
+
+    it('with the author text', () => {
+      expect(wrapper.find({ testID: 'authorText' }).props().children).toEqual(
+        reply.author
+      )
+    })
+
+    it('with the reply body text', () => {
+      expect(
+        wrapper.find({ testID: 'replyMessageText' }).props().children
+      ).toEqual(reply.body)
+    })
+
+    it('with the vote count', () => {
+      expect(
+        wrapper.find({ testID: 'replyVoteCount' }).props().children[0]
+      ).toEqual(reply.voteCount)
+    })
+  })
+
+  it('should show the placeholder image if no author profile image', () => {
+    const authorAccount = reply.authorAccount
+    authorAccount.profileImage = null
+    wrapper.setProps({
+      reply: {
+        ...reply,
+        authorAccount,
+      },
+    })
+
+    expect(wrapper.find('ProfileImageThumb').props().profileImage).toBeNull()
+    expect(wrapper).toMatchSnapshot()
+  })
+})
--- screens/RepliesScreen.js
+++ screens/RepliesScreen.js
@@ -3,15 +3,11 @@ import gql from 'graphql-tag'
 import React, { Fragment } from 'react'
 import { Query } from 'react-apollo'
 import { StyleSheet, Text, View } from 'react-native'
-import Icon from 'react-native-vector-icons/FontAwesome'
 import ApolloWrapper from '../components/ApolloWrapper'
 import Loading from '../components/Loading'
 import PageContainer from '../components/PageContainer'
-import ProfileImageThumb from '../components/ProfileImageThumb'
-import Column from '../components/layouts/Column'
-import Row from '../components/layouts/Row'
-import { ICON_BUTTON_SIZE } from '../constants'
 import { Reply as ReplyType } from '../constants/types'
+import Reply from '../components/Reply'
 
 function getReplyQuery(author, permalink) {
   return gql`
@@ -73,44 +69,6 @@ function getReplyQuery(author, permalink) {
   }
 `
 }
-type ReplyProps = {
-  reply: ReplyType,
-}
-const Reply = ({ reply }: ReplyProps) => (
-  <Column style={styles.reply}>
-    <Row style={{ width: '100%' }}>
-      <ProfileImageThumb
-        style={{ marginRight: 10 }}
-        profileImage={(reply.authorAccount || {}).profileImage}
-      />
-      <View style={styles.full}>
-        <Text style={styles.authorText}>
-          {reply.author} <Text style={styles.commentText}>{reply.body}</Text>{' '}
-        </Text>
-      </View>
-      <Icon
-        style={{ marginLeft: 10 }}
-        name="heart-o"
-        size={ICON_BUTTON_SIZE}
-        color="#4F4F4F"
-      />
-    </Row>
-    <Row style={{ marginTop: 10 }}>
-      <View style={styles.full}>
-        <Text>2 weeks ago</Text>
-      </View>
-      <View style={[styles.full, { alignItems: 'center' }]}>
-        <Text>$0.92</Text>
-      </View>
-      <View style={[styles.full, { alignItems: 'flex-end' }]}>
-        <Text>
-          {reply.voteCount} likes{' '}
-          <Icon name="reply" size={ICON_BUTTON_SIZE} color="#4F4F4F" />
-        </Text>
-      </View>
-    </Row>
-  </Column>
-)
 
 type RepliesProps = {
   replies: [ReplyType],
@@ -154,23 +112,5 @@ const RepliesScreen = (props: any) => {
     </PageContainer>
   )
 }
-
-const styles = StyleSheet.create({
-  reply: {
-    padding: 10,
-    marginTop: 10,
-    borderBottomWidth: StyleSheet.hairlineWidth,
-    borderBottomColor: '#7D7D7D',
-  },
-  full: {
-    flex: 1,
-  },
-  authorText: {
-    fontWeight: 'bold',
-  },
-  commentText: {
-    fontWeight: 'normal',
-  },
-})
 
 export default ApolloWrapper(RepliesScreen)
