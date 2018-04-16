f916fa4b64ea0219b0d146b48d2ce896c92a3fbb
Deus Duke <deusduke@gmail.com>
2018-04-15 17:20:38 -0700

    Update app to work on android
    - android doesn't like localhost, use ip
    - upgrade android gradle version to latest
    - update some JSX that works on iOS but not android
    
    [#156414904]

--- components/Loading.js
+++ components/Loading.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import { ActivityIndicator, View, Text } from 'react-native'
+import Row from './layouts/Row'
 
 const Loading = () => (
   <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-    <Text>
-      Loading posts <ActivityIndicator />
-    </Text>
+    <Row>
+      <Text>Loading posts</Text>
+      <ActivityIndicator style={{ marginLeft: 10 }} />
+    </Row>
   </View>
 )
 
